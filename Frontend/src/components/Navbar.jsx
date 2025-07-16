8e9c559a83d8d25f27f1d66155370ce9f17936d3
Preston Schnell <64565042+prestonschnell@users.noreply.github.com>
2025-07-16 10:24:39 -0500

    Added Navbar.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router';
 import './Navbar.css';
+import { X } from 'lucide-react';
+import { PlusIcon } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="navbar-container">
+        <div className='logo-container'>
+            <h1>ThinkBoard App</h1>
+        </div>
+        <div className="button-container">
+            <button>
+                <Link to="/create" className="create-note-link">
+                    <PlusIcon /> Create Note
+                </Link>
+            </button>
+        </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
